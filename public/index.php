c523583ec1c15f0dafe773dfc06d96b3d5bcee20
vaimo-wilko <wilko.nienhaus@vaimo.com>
2014-06-04 15:45:26 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -84,5 +84,4 @@ $mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : ''
 /* Run store or run website */
 $mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
 
-echo "more nonsense from trainng";
-//Mage::run($mageRunCode, $mageRunType);
+Mage::run($mageRunCode, $mageRunType);
