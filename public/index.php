1b631f552e8f9dea656ef13680119ea7826e68e6
vaimo-wilko <wilko.nienhaus@vaimo.com>
2014-06-04 15:43:39 +0300

    test commit

--- public/index.php
+++ public/index.php
@@ -84,4 +84,5 @@ $mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : ''
 /* Run store or run website */
 $mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
 
-Mage::run($mageRunCode, $mageRunType);
+echo "more nonsense from trainng";
+//Mage::run($mageRunCode, $mageRunType);
